c6be54359f7d237343d67c07d0d6904cc2f6714c
Denver Arancillo <xdba28@gmail.com>
2018-05-06 21:05:19 +0800

    added fullcalendar

--- calendar.php
+++ calendar.php
@@ -1,24 +1,33 @@
 <html>
 <head>
 	<title>Calendar</title>
+	<link rel='stylesheet' href='fullcalendar/fullcalendar.css' />
+	<script src='js/jquery-3.3.1.min.js'></script>
+	<script src='js/moment.min.js'></script>
+	<script src='fullcalendar/fullcalendar.js'></script>
 </head>
 <body>
 	<div id="calendar" style="height:800px;">
 	</div>
 </body>
 <script>
-	import Calendar from 'tui-calendar';
+$(document).ready(function()
+{
+	$(function(){
 
-	var calendar = new Calendar('#calendar', {
-  defaultView: 'month',
-  taskView: true,
-  template: {
-    monthGridHeader: function(model) {
-      var date = new Date(model.date);
-      var template = '<span class="tui-full-calendar-weekday-grid-date">' + date.getDate() + '</span>';
-      return template;
-    }
-  }
+	// page is now ready, initialize the calendar...
+
+		$('#calendar').fullCalendar({
+		// put your options and callbacks here
+
+			dayClick: function()
+			{
+				alert('a day has been clicked!');
+			}
+		})
+
+	});
+
 });
 </script>
 </html>
