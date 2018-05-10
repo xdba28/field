e0ecf6f457985f988403c0b7c2c4452d4a4f30a0
Denver Arancillo <xdba28@gmail.com>
2018-05-10 19:44:33 +0800

    added JS object and alt elseif statement

--- javascript.php
+++ javascript.php
@@ -4,6 +4,16 @@
 	<body>
 		<div id="1">
 		</div>
+		<?php
+		// alternative else if statement
+		$a = 1;
+		$b = 2;
+		if($a == 2):
+		?>
+		1
+		<?php elseif($b == 2): ?>
+		2
+		<?php endif ?>
 	</body>
 	<script>
 		const _ = (id) => document.getElementById(id);
@@ -21,5 +31,33 @@
 			"Good Morning" : "Good Afternoon";
 		_('1').innerHTML += getSalutation(10);
 		_('1').innerHTML += '<br>';
+
+		// JS object
+		var person = 
+		{
+			firstName: 'Denver',
+			lastName: 'Arancillo',
+			age: '19',
+			list: [],
+			address: 
+			{
+				street: '1173 5th street Our Lady\'s Village',
+				Municipality: 'Legazpi City',
+ 			},
+			fullName: function()
+			{
+				return this.firstName + ' ' + this.lastName;
+			}
+		}
+
+		person.list.push('asd');
+
+		person.list = '';
+
+		console.log(person.list);
+
+		var c = 1, b = 0;
+		a == b ?
+		conlos.lot('asd') : console.log('dsa')
 	</script>
 </html>
